e6e39090478f0af2eb8d037f4941f3e9cb616fee
Omar-Odeh <omarodeh.2001@hotmail.com>
2022-10-15 18:32:17 +0300

    apply cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(3001, () => {
   console.log("listening on port 3001...");
