130630aecc14e1f0a7f68f071ac0fda49c9bc2b2
Ahmed <ahmed_ad_07@hotmail.com>
2022-10-15 18:51:29 +0300

    create user route

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import { userRouter } from "./src/route/user/index.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(userRouter);
 
 app.listen(3001, () => {
   console.log("listening on port 3001...");
