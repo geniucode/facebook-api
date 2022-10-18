5b5a9e89ec0cbb13b48eb7a63525d0d4a040d72c
mohammed Faraj <function.faraj@gmail.com>
2022-10-18 17:58:50 +0300

    formate

--- dbconnection.js
+++ dbconnection.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 export const connectToDB = async () => {
   await mongoose.connect(
-    'mongodb+srv://MERNDB:Ln3jiYqKq6CA43Ia@cluster0.mbg3avh.mongodb.net/test'
-  );};
-
-
-
+    "mongodb+srv://MERNDB:Ln3jiYqKq6CA43Ia@cluster0.mbg3avh.mongodb.net/test"
+  );
+};
