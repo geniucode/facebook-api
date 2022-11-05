a23f77e92d1e570e03c9159e6476ef67a63dc5ba
mohammed Faraj <function.faraj@gmail.com>
2022-11-05 18:29:11 +0300

    changes

--- src/route/user/forgotPassword/index.js
+++ src/route/user/forgotPassword/index.js
@@ -39,39 +39,13 @@ userForgotPasswordRouter.post(
       console.log(forgetPasswordToken);
 
       if (emailExists.id) {
-        async function mailVarification() {
-          // Generate test SMTP service account from ethereal.email
-          // Only needed if you don't have a real mail account for testing
-          let testAccount = await nodemailer.createTestAccount();
-
-          // create reusable transporter object using the default SMTP transport
-          let transporter = nodemailer.createTransport({
-            host: process.env.mailHost,
-            port: process.env.mailPort,
-            secure: process.env.mailSecurity, // true for 465, false for other ports
-
-            auth: {
-              user: process.env.mailUser, // generated ethereal user
-              pass: process.env.mailPassword, // generated ethereal password
-            },
-          });
-          // send mail with defined transport object
-          let info = await transporter.sendMail({
-            from: '"Facebook Inc" forgotpassword@facebook.inc', // sender address
-            to: email, // list of receivers
-            subject: "Verification code ", // Subject line
-            text: `Dear ${email}, <br/> you're Code is  ${forgetPasswordToken} please click on the link below to enter code   `, // plain text body
-            html: `Dear ${email}, <br/> you're Code is  ${forgetPasswordToken} please click on the link below to enter code`, // html body
-          });
-
-          transporter.verify().then(console.log).catch(console.error);
-          console.log("Message sent: %s", info.messageId);
-          // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-          // Preview only available when sending through an Ethereal account
-          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-          // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-        }
+        mailVarification({
+          from: '"Facebook Inc" forgotpassword@facebook.inc', // sender address
+          to: email, // list of receivers
+          subject: "Verification code ", // Subject line
+          text: `Dear ${email}, <br/> you're Code is  ${forgetPasswordToken} please click on the link below to enter code   `, // plain text body
+          html: `Dear ${email}, <br/> you're Code is  ${forgetPasswordToken} please click on the link below to enter code`, // html body
+        });
 
         const updatePasswordToken = await User.updateOne(
           { _id: email._id },
@@ -79,7 +53,6 @@ userForgotPasswordRouter.post(
         );
         await updatePasswordToken.save();
 
-        mailVarification(); // .catch(console.error);
         res.status(STATUS_CODE.OK).send({ success: true });
       } else {
         res.status(STATUS_CODE.BadInputs).send({ success: false });
