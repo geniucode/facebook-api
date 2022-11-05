22971740cc0e2558d8e558ba90b23386d82b105e
AliAlrob <ali.abualrob2612@gmail.com>
2022-11-05 18:44:36 +0200

    valitade jwt token by post rotue

---
+++ src/route/user/validateToken/index.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken'
+import express from 'express'
+import { STATUS_CODE } from '#root/code-status';
+const accessTokenSecret = 'aliSecret';//i need to know it 
+const validateTokenRouter=express.Router();
+validateTokenRouter.post('/validate-token',(req,res)=>{
+    const authHeader=req.headers.authorization;
+    if(authHeader){
+        const token=authHeader.split(' ')[1];
+        jwt.verify(token,accessTokenSecret,(err, user) => {
+            if (err) {
+                return res.status(STATUS_CODE.Forbidden).send({seccuss:false})
+                // The HTTP 403 Forbidden response status code indicates that the
+                //  server understands the request but refuses to authorize it.
+            }
+            //req.user = user; it must if i need use token data in another route like const {username}=req.user
+           res.send({seccuss:true,user})
+        })
+    }
+    else {
+        res.status(STATUS_CODE.UnAuthorized).send({seccuss:false});
+        // indicates that the client request has not been 
+        // completed because it lacks valid authentication credentials
+    }
+
+
+
+})
+
+export {validateTokenRouter}
