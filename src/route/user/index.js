a192a71c4a382c923538f6bc1773dcbdfbfcaf59
mohammed Faraj <function.faraj@gmail.com>
2022-11-05 19:54:25 +0300

    validate token

--- src/route/user/index.js
+++ src/route/user/index.js
@@ -3,11 +3,13 @@ import { userLoginRouter } from "./login/index.js";
 import { userSignupRouter } from "./signup/index.js";
 import { userForgotPasswordRouter } from "./forgotPassword/index.js";
 import { userResetPasswordRouter } from "./resetPassword/index.js";
+import { validateTokenRouter } from "./validateToken/index.js";
 
 const userRouter = express.Router();
 userRouter.use(userLoginRouter);
 userRouter.use(userSignupRouter);
 userRouter.use(userForgotPasswordRouter);
 userRouter.use(userResetPasswordRouter);
+userRouter.use(validateTokenRouter);
 
 export { userRouter };
