0d27bb2d24be3bdff43eba73a0afbf0cd75334f5
RQawariq <rabeeqawariq@gmail.com>
2022-12-06 10:27:54 +0200

    ss

--- src/enum/react.js
+++ src/enum/react.js
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-export const reactEnum = [
-  "Like",
-  "Love",
-  "Care",
-  "Haha",
-  "Wow",
-  "Sad",
-  "Angry",
-];
-export const REACT_ENUM = {
-=======
 const reactEnum = ["Like", "Love", "Care", "Haha", "Wow", "Sad", "Angry"];
 const REACT_ENUM = {
->>>>>>> 13402955f2b3ba26471530b6e86a216813cd2b61
   like: "Like",
   love: "Love",
   care: "Care",
@@ -21,8 +8,5 @@ const REACT_ENUM = {
   sad: "Sad",
   angry: "Angry",
 };
-<<<<<<< HEAD
-=======
 
 export { reactEnum, REACT_ENUM };
->>>>>>> 13402955f2b3ba26471530b6e86a216813cd2b61
