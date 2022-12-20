923ff9bd32968d7ae2677880957584191b986547
RQawariq <rabeeqawariq@gmail.com>
2022-12-20 11:21:30 +0200

    middleware Multer

--- src/utils/middlewareMulter.js
+++ src/utils/middlewareMulter.js
@@ -1,9 +1,13 @@
-import { util } from "util";
+import util from "util";
 import Multer from "multer";
+
+const maxSize = 2 * 1024 * 1024;
 
 let processFile = Multer({
   storage: Multer.memoryStorage(),
+  limits: { fileSize: maxSize },
 }).single("file");
 
 let processFileMiddleware = util.promisify(processFile);
-module.exports = processFileMiddleware;
+
+export default { processFileMiddleware };
