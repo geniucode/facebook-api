9da8b7593402b165e3b276c9c7188624fa992da3
Anas Anbtawi <eng.anas.anbtawi@gmail.com>
2023-01-02 23:59:32 +0200

    2-JAN-2023

--- src/route/friend/index.js
+++ src/route/friend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { body } from "express-validator";
 import { validate } from "#utils/validator.js";
-import { FacebookFriend } from "#model/friend/index.js";
+import { FacebookFriend, statusConstants } from "#model/friend/index.js";
 import { User } from "#model/user/index.js";
 import { withAuth } from "../../utils/withAuth.js";
 import { STATUS_CODE } from "#root/code-status.js";
@@ -57,6 +57,124 @@ addFacebookFriendRequestRouter.post(
           return res.status(STATUS_CODE.DuplicateOrBad).send({
             success: false,
             message: "You Already sent a friend request for this person!",
+          });
+        }
+      } else {
+        return res
+          .status(STATUS_CODE.BadInput)
+          .json({ message: "you cannot add yourself " });
+      }
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(STATUS_CODE.DuplicateOrBad)
+        .send({ success: false, message: "Wrong input" });
+    }
+  }
+);
+
+addFacebookFriendRequestRouter.post(
+  "/cancel-friend-request",
+  withAuth,
+  body("recipient").isMongoId(),
+  validate,
+  async (req, res) => {
+    try {
+      const { recipient } = req.body;
+      const requester = req.user._id;
+
+      if (alreadySentFriendRequest) {
+        const cancelFriendRequest = await FacebookPost.findOneAndDelete({
+          requester,
+          recipient,
+        });
+        if (cancelFriendRequest) {
+          return res.status(STATUS_CODE.OK).send({
+            success: true,
+            message: "Friend Request was Cancelled Successfully",
+          });
+        }
+      } else {
+        return res.status(STATUS_CODE.DuplicateOrBad).send({
+          success: false,
+          message: "Friend request cannot be deleted because it's not found ",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(STATUS_CODE.DuplicateOrBad)
+        .send({ success: false, message: "Wrong input" });
+    }
+  }
+);
+
+addFacebookFriendRequestRouter.post(
+  "/ignore-friend-request",
+  withAuth,
+  body("requester").isMongoId(),
+  validate,
+  async (req, res) => {
+    try {
+      const { requester } = req.body;
+      const recipient = req.user._id;
+
+      if (alreadySentFriendRequest) {
+        const ignoreFriendRequest = await FacebookPost.findOneAndDelete({
+          requester,
+          recipient,
+        });
+        if (ignoreFriendRequest) {
+          return res.status(STATUS_CODE.OK).send({
+            success: true,
+            message: "Friend Request was ignored Successfully",
+          });
+        }
+      } else {
+        return res.status(STATUS_CODE.DuplicateOrBad).send({
+          success: false,
+          message: "Friend request cannot be ignored  because it's not found ",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(STATUS_CODE.DuplicateOrBad)
+        .send({ success: false, message: "Wrong input" });
+    }
+  }
+);
+
+addFacebookFriendRequestRouter.post(
+  "/accept-friend-request",
+  withAuth,
+  body("requester").isMongoId(),
+  validate,
+  async (req, res) => {
+    try {
+      const { requester } = req.body;
+      const recipient = req.user._id;
+      if (requester !== recipient) {
+        const pendingFriendRequest = await FacebookFriend.findOne({
+          requester,
+          recipient,
+          status: pending,
+        });
+
+        if (pendingFriendRequest) {
+          const acceptNewFriendRequest = await FacebookPost.findByIdAndUpdate(
+            pendingFriendRequest._id,
+            { recipient, requester, status: accepted }
+          );
+
+          return res.status(STATUS_CODE.OK).send({
+            success: true,
+            message: "Friend is successfully added !",
+          });
+        } else {
+          return res.status(STATUS_CODE.DuplicateOrBad).send({
+            success: false,
+            message: "You are already a friend with this person!",
           });
         }
       } else {
