df72155e464f5148a1e1d7b6420fecc7d020da41
aliabualrob12 <83252393+aliabualrob12@users.noreply.github.com>
2023-01-03 17:22:13 +0200

    get all user posts router

--- src/route/facebookPost/index.js
+++ src/route/facebookPost/index.js
@@ -3,7 +3,8 @@ import { addFacebookPostRouter } from "./addFacebookPost/index.js";
 import { getFacebookPostRouter } from "./getFacebookPost/index.js";
 import { deleteFacebookPostRouter } from "./deleteFacebookPost/index.js";
 import { updateFacebookPostRouter } from "./updateFacebookPost/index.js";
-import {  getAllPostsRouter } from "./getAllFacebookPosts/index.js";
+import { getAllPostsRouter } from "./getAllFacebookPosts/index.js";
+import { getAllUserPostsRouter } from "./getAlluserPosts/index.js";
 
 const facebookPostRouter = express.Router();
 facebookPostRouter.use(addFacebookPostRouter);
@@ -11,5 +12,6 @@ facebookPostRouter.use(getFacebookPostRouter);
 facebookPostRouter.use(deleteFacebookPostRouter);
 facebookPostRouter.use(updateFacebookPostRouter);
 facebookPostRouter.use(getAllPostsRouter);
+facebookPostRouter.use(getAllUserPostsRouter);
 
 export { facebookPostRouter };
