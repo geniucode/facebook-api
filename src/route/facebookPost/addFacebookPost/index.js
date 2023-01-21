c7496941d623f8c5b3ea290fdab76a30f361c63e
aliabualrob12 <83252393+aliabualrob12@users.noreply.github.com>
2023-01-21 05:13:33 +0200

    make share post

--- src/route/facebookPost/addFacebookPost/index.js
+++ src/route/facebookPost/addFacebookPost/index.js
@@ -18,8 +18,16 @@ addFacebookPostRouter.post(
   async (req, res) => {
     try {
       const member = req.user;
-      const createdBy = member._id;
-      const { user, postBody, postImg } = req.body;
+      // const createdBy = member._id;
+      const {
+        user,
+        postBody,
+        postImg,
+        sharedBy,
+        createdBy = member._id,
+        shareNumber = 0,
+        isCopy = false,
+      } = req.body;
       const userFound = await User.findOne({ _id: user });
       if (userFound) {
         const newPost = new FacebookPost({
@@ -27,6 +35,9 @@ addFacebookPostRouter.post(
           postBody,
           postImg,
           createdBy,
+          sharedBy,
+          shareNumber,
+          isCopy,
         });
         await newPost.save();
         res
