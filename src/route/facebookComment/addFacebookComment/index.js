a652a89dba0d1eae5001818f8587e170eeca90fe
Ahmed <ahmed_ad_07@hotmail.com>
2023-01-21 18:51:55 +0300

    add coment end point

--- src/route/facebookComment/addFacebookComment/index.js
+++ src/route/facebookComment/addFacebookComment/index.js
@@ -4,37 +4,35 @@ import { validate } from "#utils/validator.js";
 import { STATUS_CODE } from "#root/code-status.js";
 import { User } from "../../../model/user/index.js";
 import { FacebookComment } from "../../../model/facebookComment/index.js";
+import { withAuth } from "../../../utils/withAuth.js";
 
 const addFacebookCommentRouter = express.Router();
 
 addFacebookCommentRouter.post(
   "/add-comment",
+  withAuth,
   body("comment")
     .notEmpty()
     .withMessage("comment is required")
     .isString()
     .withMessage("Please enter a valid comment"),
-    body("commentImage")
-    .notEmpty()
-    .withMessage("Image is required")
-    .isString()
-    .withMessage("Please enter a valid image for comment "), 
+
   body("userId").notEmpty().withMessage("userId required"),
   body("postId").notEmpty().withMessage("postId is required"),
 
   validate,
   async (req, res) => {
     try {
-      const { comment,commentImage, userId, postId } = req.body;
-      const newcomment = new facebookcomment({ comment,commentImage, userId, postId });
-      const findcomment = await facebookcomment
-        .findOne({ postId })
-        .lean();
-      if (findcomment) {
-        await facebookComment.deleteOne({ commentId });
-      }
+      const member = req.user;
+      const createdBy = member._id;
+      const { comment, postId } = req.body;
+      const newcomment = new FacebookComment({
+        comment,
+        postId,
+        commentBy: createdBy,
+      });
       await newcomment.save();
-      res.send({ success: true, message: "comment is be saved" });
+      res.send({ success: true, message: "comment saved" });
     } catch (error) {
       res.status(STATUS_CODE.BadInput).send({
         success: false,
